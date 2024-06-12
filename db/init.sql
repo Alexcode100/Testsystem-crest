8a9565f6995af4a1b896f31f76d906dc71b9ab5c
Alexcode100 <89846069+Alexcode100@users.noreply.github.com>
2024-06-12 10:31:44 +0200

    Added SQL script to initialize the database

---
+++ db/init.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS TestSystemDBCrest;
+
+-- Use the newly created database
+USE TestSystemDBCrest;
+
+-- Drop Tables if they exist
+DROP TABLE IF EXISTS TestResults;
+DROP TABLE IF EXISTS TestsTable;
+
+-- Create TestsTable
+CREATE TABLE TestsTable (
+    TestID INT AUTO_INCREMENT PRIMARY KEY,
+    TestDate DATE NOT NULL,
+    TestTime TIME NOT NULL,
+    DeviceType VARCHAR(255) NOT NULL,
+    IDSensor INT NOT NULL
+);
+
+-- Create TestResults
+CREATE TABLE TestResults (
+    TestSpecificID INT AUTO_INCREMENT PRIMARY KEY,
+    TestID INT NOT NULL,
+    MeasurementType VARCHAR(255) NOT NULL,
+    MeasurementValue VARCHAR(255) NOT NULL,
+    Result VARCHAR(255) NOT NULL,
+    FOREIGN KEY (TestID) REFERENCES TestsTable(TestID) ON DELETE CASCADE
+);
